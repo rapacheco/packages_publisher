ba6fabd8836da6025d3516ba946f837accf99d8e
Rafael Ribeiro <rapachecobeiro@hotmail.com>
2021-12-10 20:05:19 -0500

    Add project files.

---
+++ packages_publisher/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace packages_publisher
+{
+    public class Awesome
+    {
+        static void WhoIs()
+        {
+            Console.WriteLine("You are awesome!");
+        }
+    }
+}
